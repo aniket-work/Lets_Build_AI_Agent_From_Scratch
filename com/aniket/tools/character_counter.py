afd322dd4250b24191a42f6eff1d1161c3c2ede7
worka <study.aniket.hingane@gmail.com>
2024-07-26 23:53:03 -0400

    added AI Agents tools

--- com/aniket/tools/character_counter.py
+++ com/aniket/tools/character_counter.py
@@ -1,6 +1,6 @@
 def count_characters(sentence):
     """
-    Count the total number of characters in a given sentence.
+    Count the total number of characters in a given sentence
 
     Args:
     sentence (str): The input sentence to analyze.
